74e62a05c4c8df4dfc77b0d7ed020cab486ac3a5
Rushil Umaretiya <rushilwiz@gmail.com>
2021-02-07 17:29:11 -0500

    feat: placed site under construction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shaanta Inc's site is currently under development.</h1>
+      <h3>Please enjoy this image in the meantime:</h3>
+      <img src="https://lh3.googleusercontent.com/TRyKNTgQWp6ZxZ6SvR9-MN1k_Pxfakt8PbjVUguk3IMf9xPc4aggfna4nBrPCgslQ_-57pTWywQcNZNCeobtPiBnd7MGzHs_AmFwRUBTqjgmApwBghdOppxezQM9duu9SEZ5jsx7wXyMQfSdrtPV3MzT4a9pkYnQprYPryvP6eSvcTL_mpiWBMMoEAxv5c2bYwk2IQY4aqS6uEMQiefySPvY4GMZrDyXig5NsK9lpYGgXyrhGxmYdTL6ZqTz6DTGUlJMkNxhdOofqeOFzfCXBPiUkT6TrPwjXERhkQqnRffmkysQ0p8YZ-EbB2TXnmvKpfWTZaDE5P2hvicUrVYiAIp90_pdN-oujsX_jiXnSVdemyvP4lu4rowveeE7SLwE4ZaveB52Oq7CFZogCc96Dus8EYzEt39fLtqvzfO9fNBK0QzTvbLgfV05bDHreWcKSwLuojuhZLQ4WahrA9AnS_doZDH-WM_2H1-t3D2K2ZLs6SBw48b0ceYIJccqxoTlHmuvimjE7Sz2FZ15V09BZc2JKLTNqaWN9yRpbMRDvCqQpGzP2jYasYEx-Tage-6vwK6Pl8ORY60IFSggOwZfoJgaU3GGlvhOmZMtf8izKyfNW9iL6mjGhPpdaGolMd9rH3of6xZEE4ceWLpPQRnm_rg5R3mEyMbmgH6XNOoOewX320vaZLE3UhbNmviNeA=w1247-h937-no" />
     </div>
   );
 }
