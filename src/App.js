f75f48bb0743af3e3cd4bc0ac2fac23c71fdc992
Rushil Umaretiya <rushilwiz@gmail.com>
2021-02-20 12:19:55 -0500

    fix: fixed under construction image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import family from "./components/assets/img/family.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>Shaanta Inc's site is currently under development.</h1>
       <h3>Please enjoy this image in the meantime:</h3>
-      <img src="https://lh3.googleusercontent.com/TRyKNTgQWp6ZxZ6SvR9-MN1k_Pxfakt8PbjVUguk3IMf9xPc4aggfna4nBrPCgslQ_-57pTWywQcNZNCeobtPiBnd7MGzHs_AmFwRUBTqjgmApwBghdOppxezQM9duu9SEZ5jsx7wXyMQfSdrtPV3MzT4a9pkYnQprYPryvP6eSvcTL_mpiWBMMoEAxv5c2bYwk2IQY4aqS6uEMQiefySPvY4GMZrDyXig5NsK9lpYGgXyrhGxmYdTL6ZqTz6DTGUlJMkNxhdOofqeOFzfCXBPiUkT6TrPwjXERhkQqnRffmkysQ0p8YZ-EbB2TXnmvKpfWTZaDE5P2hvicUrVYiAIp90_pdN-oujsX_jiXnSVdemyvP4lu4rowveeE7SLwE4ZaveB52Oq7CFZogCc96Dus8EYzEt39fLtqvzfO9fNBK0QzTvbLgfV05bDHreWcKSwLuojuhZLQ4WahrA9AnS_doZDH-WM_2H1-t3D2K2ZLs6SBw48b0ceYIJccqxoTlHmuvimjE7Sz2FZ15V09BZc2JKLTNqaWN9yRpbMRDvCqQpGzP2jYasYEx-Tage-6vwK6Pl8ORY60IFSggOwZfoJgaU3GGlvhOmZMtf8izKyfNW9iL6mjGhPpdaGolMd9rH3of6xZEE4ceWLpPQRnm_rg5R3mEyMbmgH6XNOoOewX320vaZLE3UhbNmviNeA=w1247-h937-no" />
+      <img src="{family}" />
     </div>
   );
 }
